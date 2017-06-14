5f50989200786dcae44f890cf426a6f5d0f93527
yann <yann.boisselier@gmail.com>
2017-06-14 16:09:17 +0200

    refacto

--- app.js
+++ app.js
@@ -5,107 +5,99 @@ const fse = require('fs-extra');
 const graphql = require('graphql')
 const _ = require('lodash')
 
-const config = parseConfigFile('./config')
+class App {
 
-function getEnv() {
-  const env = process.env.NODE_ENV
-  return env
-}
-
-/**
- * Merge default config and current environment config in a single objetct
- * @param {string} relative path to JSON config file 
- */
-function parseConfigFile(file) {
-  let activeconfig = {}
-  const env = getEnv()
-  const config = require("./" + file)
-  activeconfig = config['*']
-  for (const property in config[env]) {
-    activeconfig[property] = config[env][property]
+  constructor() {
+    this.config = this.parseConfigFile('./config')
   }
-  return activeconfig
-}
 
-/**
- * 
- * @param {object} mongoose 
- * @param {string} mongoDBUri 
- */
-function connectToMongoDB(mongoose, mongoDBUri) {
-  mongoose.Promise = global.Promise
-  return mongoose.connect(config.mongodb_uri).then(() => {
-    console.log("connected successfully to 'mongodb://localhost/yineo' ! ")
-  }).catch((e) => {
-    console.log(e)
-  })
-}
+  parseConfigFile(file) {
+    let activeconfig = {}
+    const env = this.getEnv()
+    const config = require("./" + file)
+    activeconfig = config['*']
+    for (const property in config[env]) {
+      activeconfig[property] = config[env][property]
+    }
+    return activeconfig
+  }
 
-/**
- * Collect graphQL schema definition from modules to build 
- * our graphQL schema
- * @param {array} modules
- */
-function buildGraphQLSchemaFromModules(modules, modulesPath) {
-  // collect query fields from modules
-  let queryFields = {}
-  for (const moduleId in modules) {
-    for (let filepath of modules[moduleId].plugins.graphQL) {
-      filepath = ['.', modulesPath, moduleId, filepath].join('/')
-      let schemaFragment = require(filepath)
-      for (const property in schemaFragment.queryFields) {
-        queryFields[property] = schemaFragment.queryFields[property]
+  /**
+   * 
+   * @param {object} mongoose 
+   * @param {string} mongoDBUri 
+   */
+  connectToMongoDB(mongoose, mongoDBUri) {
+    mongoose.Promise = global.Promise
+    return mongoose.connect(mongoDBUri).then(() => {
+      console.log("connected successfully to 'mongodb://localhost/yineo' ! ")
+    }).catch((e) => {
+      console.log(e)
+    })
+  }
+
+  getEnv() {
+    return process.env.NODE_ENV
+  }
+
+  /**
+   * Collect graphQL schema definition from modules to build 
+   * our graphQL schema
+   * @param {array} modules
+   */
+  buildGraphQLSchemaFromModules(modules, modulesPath) {
+    // collect query fields from modules
+    let queryFields = {}
+    for (const moduleId in modules) {
+      for (let filepath of modules[moduleId].plugins.graphQL) {
+        filepath = ['.', modulesPath, moduleId, filepath].join('/')
+        let schemaFragment = require(filepath)
+        for (const property in schemaFragment.queryFields) {
+          queryFields[property] = schemaFragment.queryFields[property]
+        }
       }
     }
+
+    // Entry points : les "resolvers"
+    const queryType = new graphql.GraphQLObjectType({
+      name: 'Query',
+      // les clefs de "fields" contiennent les points d'entrée pour notre API en http GET
+      fields: queryFields
+    });
+
+    const schema = new graphql.GraphQLSchema({
+      query: queryType
+    })
+
+    return schema
   }
 
-  // Entry points : les "resolvers"
-  const queryType = new graphql.GraphQLObjectType({
-    name: 'Query',
-    // les clefs de "fields" sont les points d'entrée pour notre API en http GET
-    fields: queryFields
-  });
-
-  const schema = new graphql.GraphQLSchema({
-    query: queryType
-  })
-
-  return schema
-}
-
-function startServer(graphQLSchema) {
-    const app = express()
-    app.use('/graphql', graphqlHTTP({
-      schema: graphQLSchema,
-      rootValue: global,
-      graphiql: true,
-    }))
-    app.listen(config.httpServerPort)
+  /**
+   * @param {string} modulesPath
+   */
+  async discoverModules(modulesPath) {
+    const modules = {}
+    const items = await fse.readdir(modulesPath)
+    for (var i = 0; i < items.length; i++) {
+      const modulePath = modulesPath + "/" + items[i]
+      const module = require("./" + modulePath)
+      module.path = modulePath
+      modules[items[i]] = module
+    }
+    return modules
   }
 
-/**
- * @param {string} modulesPath
- */
-async function discoverModules(modulesPath) {
-  const modules = {}
-  const items = await fse.readdir(modulesPath)
-  for (var i = 0; i < items.length; i++) {
-    const modulePath = modulesPath + "/" + items[i]
-    const module = require("./" + modulePath)
-    module.path = modulePath
-    modules[items[i]] = module
+  /**
+   * Run our app
+   */
+  async start() {
+    this.modules = await this.discoverModules(this.config.modulesPath)
+    this.graphQLSchema = this.buildGraphQLSchemaFromModules(this.modules, this.config.modulesPath)
+    await this.connectToMongoDB(mongoose, this.config.mongodb_uri)
+    this.express = this.startExpress(this.graphQLSchema)
+    return this
   }
-  return modules
+
 }
 
-async function start() {
-    const modules = await discoverModules(config.modulesPath)
-    const graphQLSchema = buildGraphQLSchemaFromModules(modules, config.modulesPath)
-    await connectToMongoDB(mongoose, config.mongodb_uri)
-    startServer(graphQLSchema)
-}
-
-module.exports = {
-  config,
-  start
-}
+module.exports = App
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 /**
  * Bootstrap our app
  */
-require('./App').start().catch((e) => console.log(e))
+//require('./App').start().catch((e) => console.log(e))
+const App = require('./App')
+new App().start()
+.then(e => console.log(App))
+.catch(e => console.log(e.message))
